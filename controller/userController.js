01e290cb581bbdcd834a927c4a64f48e226a2aca
Nibinh <haridasnibin@gmail.com>
2023-11-20 12:28:45 +0530

    pagecount

--- controller/userController.js
+++ controller/userController.js
@@ -1,5 +1,6 @@
 const { json } = require("express");
 const User = require("../models/user");
+const user = require("../models/user");
 
 const addUser = async (req, res) => {
   try {
@@ -54,17 +55,17 @@ const getAllUsers = async (req, res) => {
     }
     //paginatoin
     const page = req.query.page * 1 || 1;
-    const limit = req.query.limit * 1 || 7;
+    const limit = req.query.limit * 1 || 6;
     const skip = (page - 1) * limit;
     query = query.skip(skip).limit(limit);
 
     if (req.query.page) {
       const userCount = await User.countDocuments();
       if (skip >= userCount) {
-        console.log(userCount);
         return res.status(400).send("Page not Found");
       }
     }
+    //search
     const search = {};
     if (req.query.search) {
       const regexQuery = new RegExp(req.query.search, "i");
@@ -74,7 +75,17 @@ const getAllUsers = async (req, res) => {
 
     const users = await query;
 
-    return res.status(200).json({ message: "Data send", users });
+    ////////
+    let pageNum = null;
+    let pageRemin = null;
+    const userCount = await User.countDocuments();
+    pageNum = Math.floor(userCount / 6);
+    pageRemin = userCount % 6;
+
+    if (pageRemin) {
+      pageNum = ++pageNum;
+    }
+    return res.status(200).json({ message: "Data send", users, pageNum });
   } catch (error) {
     console.log(error);
     res.status(500).json({ error: "An error occured" + error.message });
